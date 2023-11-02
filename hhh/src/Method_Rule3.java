e38b7694b4ebe20d8e8ade1ce63b4db02ecceb1a
Ramachandra-2k96 <149596008+Ramachandra-2k96@users.noreply.github.com>
2023-11-02 17:26:08 +0530

    Add files via upload

---
+++ hhh/src/Method_Rule3.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Method_Rule3 
+{
+	public static Object rule3(List<String> list1) {
+		List<String> used = new ArrayList<>();
+		used=list1;
+		Scanner sc1=new Scanner(System.in);
+		String input1=sc1.nextLine();
+		String temp1="";
+		Boolean isdisqualified=false;
+		int len1=used.size();
+		
+		for(int i=0; i<len1; i++) {
+			temp1=used.get(i);
+			if(input1.equals(temp1)) {
+				System.out.println("Player is disqualified due to Rule3");
+				isdisqualified=true;
+			}
+		}
+		Object[] obj=new Object[2];
+		obj[0]=isdisqualified;
+		obj[1]=input1;
+		return obj;	
+	}
+}
--- hhh/src/first.java
+++ hhh/src/first.java
@@ -5,12 +5,89 @@ import java.util.List;
 import java.util.Scanner;
 
 public class first {
+	public static char alpha[]=new char[26];
+	
+	
+	public static void generate_alphabet()
+	{
+		for (int j = 97,i=0; j <= 122; j++,i++) {
+			alpha[i]=(char)j;
+		}
+	}
+	
+	
+	public static List<List<String>> make_group(List<String> a,List<List<String>>b)
+	{
+		for(int j=0;j<26;j++)
+		{
+			List <String>temp= new ArrayList<>();
+			for(int i=0;i<a.size();i++)
+			{
+			
+				if(a.get(i).charAt(0) == alpha[j])
+				{
+					temp.add(a.get(i));
+				}
+			}
+			b.add(temp);
+		}
+		return b;
+	}
+	
+	
+	
+	private static void Play_the_game(List<String>Used_words,List<List<String>>Same_letter_words, Method_Rule3 obj,String lastword) {
+		String input;
+		while(true) {
+			System.out.print("YOU : ");
+			Object[] o1=(Object[]) obj.rule3(Used_words); 
+			input = o1[1].toString();
+			Boolean isdisqualified =(Boolean) o1[0];
+			Used_words.add(input);
+			
+			if(isdisqualified) 
+			{
+				System.out.println("You lost the game");
+				System.exit(0);
+			}
+			
+			if (lastword.isBlank() || lastword.charAt(lastword.length() - 1) == input.charAt(0)) 
+			{
+				int len1 = input.length();
+				char last_letter= input.charAt(len1-1);
+				for(int i=0;i<Same_letter_words.size();i++)
+				{
+					if(Same_letter_words.get(i).isEmpty())
+					{
+						System.out.println("Computer : I have no word left");
+						System.out.println("You won");
+						System.exit(0);
+					}
+					else if(Same_letter_words.get(i).get(0).charAt(0) == last_letter)
+					{
+						System.out.println("Computer : "+Same_letter_words.get(i).get(0)+" with length "+Same_letter_words.get(i).get(0).length()+"\n");
+						Used_words.add(Same_letter_words.get(i).get(0));
+						lastword = Same_letter_words.get(i).remove(0);
+						break;
+					}
+				}
+			}
+			else {
+				System.out.println("You loose");
+				System.exit(0);
+			}
+		}
+	}	
 
-	public static void main(String[] args) throws FileNotFoundException 
+	
+	
+	
+public static void main(String[] args) throws FileNotFoundException 
 	{
 		List <String>words= new ArrayList<>();
+		List <String>Used_words= new ArrayList<>();
 		List <List <String>>Same_letter_words= new ArrayList<>();
-		String fileinfo ;
+		String fileinfo ,lastword;
 		File f1 = new File("C:/Users/ramac/Downloads/words.txt");
 		Scanner sc= new Scanner(f1);
 		while(sc.hasNextLine())
@@ -18,33 +95,11 @@ public class first {
 			fileinfo = sc.nextLine();
 			words.add(fileinfo);
 		}
-		char alpha[]= {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-		for(int j=0;j<26;j++)
-			{
-			List <String>temp= new ArrayList<>();
-			for(int i=0;i<words.size();i++)
-			{
-				
-				if(words.get(i).charAt(0) == alpha[j])
-				{
-					temp.add(words.get(i));
-				}
-			}
-			Same_letter_words.add(temp);
-		}
-		Scanner sc1= new Scanner(System.in);
-		System.out.print(" You : ");
-		String input = sc1.next();
-		int len1 = input.length();
-		char last_letter= input.charAt(len1-1);
-		for(int i=0;i<Same_letter_words.size();i++)
-		{
-			if(Same_letter_words.get(i).get(0).charAt(0) == last_letter)
-			{
-				System.out.println("Computer : "+Same_letter_words.get(i).get(0)+" with length "+Same_letter_words.get(i).get(0).length());
-				break;
-			}
-		}
+		generate_alphabet();
+		Same_letter_words = make_group(words,Same_letter_words);
+		lastword = " ";
+		Method_Rule3 obj=new Method_Rule3(); 
+		Play_the_game(Used_words,Same_letter_words,obj,lastword);
 	}
-
 }
+
